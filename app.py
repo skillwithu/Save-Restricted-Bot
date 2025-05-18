19b11307032df67b80f87d36c4ddf7ae71c389b7
skillwithu <skillwithu@gmail.com>
2025-05-18 11:57:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,5 +16,5 @@ def home():
 </style>"""
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='8.0.8.0', port=port)
